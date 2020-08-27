98a746f61eb7233553cd823d61e24ae7bbbf82b5
Adrian Eguez <eadepto@hotmail.com>
2020-08-27 14:34:48 -0500

    vistas

--- 01-http/introduccion/src/usuario/usuario.controller.ts
+++ 01-http/introduccion/src/usuario/usuario.controller.ts
@@ -204,10 +204,34 @@ export class UsuarioController {
     ) {
         const nombreControlador = 'Adrian';
         res.render(
-            'ejemplo', // Nombre de la vista (archivo)
+            'usuario/ejemplo', // Nombre de la vista (archivo)
             { // Parametros de la vista
                 nombre: nombreControlador,
             })
+    }
+
+    @Get('vista/faq')
+    faq(
+        @Res() res
+    ) {
+
+        res.render('usuario/faq')
+    }
+
+    @Get('vista/inicio')
+    inicio(
+        @Res() res
+    ) {
+
+        res.render('usuario/inicio')
+    }
+
+    @Get('vista/login')
+    login(
+        @Res() res
+    ) {
+
+        res.render('usuario/login')
     }
 
 
